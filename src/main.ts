28cc10b0e0259a97ce5193603ab8f5faba8dd575
vanilla-dd <freeidlll6@gmail.com>
2024-04-11 13:36:13 +0530

    final: completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 import './style.css'
+import {
+    createIcons,
+    UsersRound,
+    GitFork,
+    CircleDot,
+    Star,
+    Github,
+} from 'lucide'
+
+createIcons({
+    icons: {
+        UsersRound,
+        GitFork,
+        CircleDot,
+        Star,
+        Github,
+    },
+})
